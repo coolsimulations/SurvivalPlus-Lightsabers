0ae95d57caf95bc44eeb0f64057f1f3760d6011b
coolsim <coolsimulations@gmail.com>
2020-06-23 11:52:25 +1000

    More Updates to 0.1.4 (Credit to Sushi_Witch for models)

--- src/main/java/net/coolsimulations/Lightsaber/Lightsaber.java
+++ src/main/java/net/coolsimulations/Lightsaber/Lightsaber.java
@@ -8,6 +8,7 @@ import net.coolsimulations.Lightsaber.proxy.ClientProxy;
 import net.coolsimulations.Lightsaber.proxy.CommonProxy;
 import net.coolsimulations.Lightsaber.util.LightsaberSwordBlocking;
 import net.coolsimulations.SurvivalPlus.api.SPCompatibilityManager;
+import net.coolsimulations.SurvivalPlus.api.SPReference;
 //import net.minecraft.world.gen.feature.structure.StructureIO;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DistExecutor;
@@ -17,19 +18,20 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(value = Reference.MOD_ID)
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class Lightsaber {
-	
+
 	public static CommonProxy proxy = (CommonProxy) DistExecutor.runForDist(() -> ClientProxy::new, () -> CommonProxy::new);
-	
+
 	private static Lightsaber instance;
-	
+
 	public static Lightsaber getInstance()
-	    {
-	        return instance;
-	    }
-	
+	{
+		return instance;
+	}
+
 	public Lightsaber() {
-		
+
 		MinecraftForge.EVENT_BUS.register(new LightsaberEventHandler());
+		SPReference.MOD_ADDON_NAMES.add("sp.lightsaber.name");
 		LightsaberUpdateHandler.init();
 		LightsaberItems.init();
 		LightsaberItems.register();
@@ -39,10 +41,10 @@ public class Lightsaber {
 		//StructureIO.registerStructureComponent(StructureVillageJediHut.class, Reference.MOD_ID+":jediHutStructure"); //temp till forge pull request #6142 is resolved
 		//LightsaberVillagers.registerVillagers(); //temp till forge issue #6112 is resolved
 		LightsaberSoundHandler.init();
-		
+
 		if(SPCompatibilityManager.isSwordBlockingLoaded()) {
 			LightsaberSwordBlocking.init();
 		}
 	}
-	
+
 }
