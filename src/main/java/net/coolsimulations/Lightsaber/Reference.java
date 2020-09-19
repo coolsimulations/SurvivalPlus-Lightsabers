12d118e8de3e0782bfc08db71ccf8c74a0ea00ef
coolsim <coolsimulations@gmail.com>
2020-09-19 12:23:34 +1000

    Updated to 1.16.3

--- src/main/java/net/coolsimulations/Lightsaber/Reference.java
+++ src/main/java/net/coolsimulations/Lightsaber/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 	public static final String MOD_ID = "lightsaber";
 	public static final String MOD_NAME = "SurvivalPlus Lightsabers";
 	public static final String VERSION = "0.1.4b";
-	public static final String ACCEPTED_VERSIONS = "[1.16, 1.16.1]";
+	public static final String ACCEPTED_VERSIONS = "[1.16, 1.16.1, 1.16.3]";
 	public static final String CLIENT_PROXY_CLASS = "net.coolsimulations.Lightsaber.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "net.coolsimulations.Lightsaber.proxy.ServerProxy";
 	public static final String DEPENDENCIES = "required-after:survivalplus@[0.1.3b,);";
