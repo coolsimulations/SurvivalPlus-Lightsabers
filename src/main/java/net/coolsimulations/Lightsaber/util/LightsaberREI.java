f5522e5642aa5f56e5af157c4fbb31c23eb8e45a
coolsim <coolsimulations@gmail.com>
2021-07-27 11:18:14 +1000

    Added REI Support

---
+++ src/main/java/net/coolsimulations/Lightsaber/util/LightsaberREI.java
@@ -0,0 +1,33 @@
+package net.coolsimulations.Lightsaber.util;
+
+import me.shedaniel.rei.api.EntryRegistry;
+import me.shedaniel.rei.api.EntryStack;
+import me.shedaniel.rei.api.plugins.REIPluginV0;
+import net.coolsimulations.Lightsaber.item.ItemLightsaber;
+import net.coolsimulations.Lightsaber.Reference;
+import net.fabricmc.loader.api.SemanticVersion;
+import net.fabricmc.loader.util.version.VersionParsingException;
+import net.minecraft.util.Identifier;
+
+public class LightsaberREI implements REIPluginV0 {
+
+	@Override
+	public Identifier getPluginIdentifier() {
+		return new Identifier(Reference.MOD_ID, "rer_plugin");
+	}
+	
+	@Override
+    public SemanticVersion getMinimumVersion() throws VersionParsingException {
+        return SemanticVersion.parse("0.1.6");
+    }
+	
+	@Override
+    public void registerEntries(EntryRegistry entryRegistry) {
+		for(EntryStack stack : entryRegistry.getStacksList()) {
+			if(stack.getItem() instanceof ItemLightsaber) {
+				entryRegistry.getStacksList().remove(stack);
+			}
+		}
+	}
+
+}
